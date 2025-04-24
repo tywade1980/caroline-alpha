929d3eb20477fd862c5a5829b34c571f7654f7f4
tywade1980 <tyler@dublinremodelingservices.net>
2025-04-24 10:49:52 -0400

    Delete functions/index.js

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-
-admin.initializeApp();
-
-exports.helloCaroline = functions.https.onRequest((req, res) => {
-  res.status(200).send("Hello Caroline, your cloud function is live and running!");
-});
